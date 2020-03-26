9a9994f9bd3c21faecf71af0bc6e37336a56275f
Paul Terry <pterry175@gmail.com>
2020-03-26 09:18:56 -0400

    Update biggest.py

--- biggest.py
+++ biggest.py
@@ -12,17 +12,17 @@ def biggest(x,y,z):
 
 
 
-print biggest(3, 6, 9)
+print (biggest(3, 6, 9))
 #>>> 9
 
-print biggest(6, 9, 3)
+print (biggest(6, 9, 3))
 #>>> 9
 
-print biggest(9, 3, 6)
+print (biggest(9, 3, 6))
 #>>> 9
 
-print biggest(3, 3, 9)
+print (biggest(3, 3, 9))
 #>>> 9
 
-print biggest(9, 3, 9)
+print (biggest(9, 3, 9))
 #>>> 9
