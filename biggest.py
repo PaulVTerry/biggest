1f14c0be5fb25f956b28e6d6455352c40955d938
Paul Terry <pterry175@gmail.com>
2020-03-26 09:14:23 -0400

    Add files via upload

---
+++ biggest.py
@@ -0,0 +1,28 @@
+def biggest(x,y,z):
+    if x > y:
+        if x > z:
+            return x
+        else:
+            return z
+    else:
+        if y > z:
+            return y
+        else:
+            return z
+
+
+
+print biggest(3, 6, 9)
+#>>> 9
+
+print biggest(6, 9, 3)
+#>>> 9
+
+print biggest(9, 3, 6)
+#>>> 9
+
+print biggest(3, 3, 9)
+#>>> 9
+
+print biggest(9, 3, 9)
+#>>> 9
